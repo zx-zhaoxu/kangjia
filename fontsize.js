8eef64c4f5e6a50c01db0bb33df9dd9308dddefd
zx-zhaoxu <273801974@qq.com>
2017-10-11 11:54:35 +0800

    Add files via upload

---
+++ fontsize.js
@@ -0,0 +1,1 @@
+ !function(a){function b(){var b=a.document,c=b.documentElement,d=c.getBoundingClientRect().width;document.documentElement.style.fontSize=100*(d/720)+"px";console.log(d);}window.addEventListener("DOMContentLoaded",function(){b()},!1),window.addEventListener("resize",function(){b()}),b()}(window);
